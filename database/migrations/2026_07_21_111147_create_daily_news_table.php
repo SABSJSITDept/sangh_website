755d0fc87bd4e95fa1cbe75ce29ecfeaf00be71b
sabsjs <sabsjsgithubacc@gmail.com>
2026-07-21 16:43:30 +0530

    news update

---
+++ database/migrations/2026_07_21_111147_create_daily_news_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('daily_news', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('anchal_id')->nullable();
+            $table->string('photo')->nullable();
+            $table->date('date')->nullable();
+            $table->unsignedBigInteger('city_id')->nullable();
+            $table->unsignedBigInteger('state_id')->nullable();
+            $table->unsignedBigInteger('local_sangh_id')->nullable();
+            $table->integer('like_count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('daily_news');
+    }
+};
